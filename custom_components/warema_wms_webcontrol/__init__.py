38bab8ac11899ce1c46beccf948a4a0c19cc10e3
Hubert Becker <hu@hjpb.de>
2025-01-20 21:58:49 +0100

    Update __init__.py

--- custom_components/warema_wms_webcontrol/__init__.py
+++ custom_components/warema_wms_webcontrol/__init__.py
@@ -1,1 +1,23 @@
-"""Warmema wms webcontrol integration for shades"""
+import logging
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+_LOGGER = logging.getLogger(__name__)
+
+DOMAIN = "warema_wms_control"
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    """Set up the custom cover integration."""
+    hass.data[DOMAIN] = {}
+    return True
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up the custom cover from a config entry."""
+    hass.data[DOMAIN][entry.entry_id] = entry.data
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload a config entry."""
+    hass.data[DOMAIN].pop(entry.entry_id)
+    return True
