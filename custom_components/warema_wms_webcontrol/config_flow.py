fc039529fda7206f303b57f3a6c24838d3f445f9
Hubert Becker <hu@hjpb.de>
2025-01-21 14:38:29 +0100

    Update config_flow.py

--- custom_components/warema_wms_webcontrol/config_flow.py
+++ custom_components/warema_wms_webcontrol/config_flow.py
@@ -1,6 +1,18 @@
-from homeassistant import config_entries
+from __future__ import annotations
+
+import logging
+from typing import Any
+
 import voluptuous as vol
-from .const import DOMAIN
+
+from homeassistant import config_entries, exceptions
+from homeassistant.core import HomeAssistant
+
+from .const import DOMAIN  # pylint:disable=unused-import
+from .hub import Hub
+
+_LOGGER = logging.getLogger(__name__)
+
 
 class WaremaWMSWebControlConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Warema WMS WebControl."""
@@ -12,8 +24,18 @@ class WaremaWMSWebControlConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         errors = {}
 
         if user_input is not None:
-            url = user_input.get("url")
+            url = user_input.get("webcontrol_server_addr")
             update_interval = user_input.get("update_interval")
+
+            hub = Hub(hass, data["webcontrol_server_addr"])
+            # The dummy hub provides a `test_connection` method to ensure it's working
+            # as expected
+            result = await hub.test_connection()
+            if not result:
+                # If there is an error, raise an exception to notify HA that there was a
+                # problem. The UI will also show there was a problem
+                raise CannotConnect
+
 
             # Validate URL and update interval
             if not self._is_valid_url(url):
@@ -26,7 +48,7 @@ class WaremaWMSWebControlConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         # Schema for the form
         data_schema = vol.Schema(
             {
-                vol.Required("url", default="http://192.168.1.100"): str,
+                vol.Required("webcontrol_server_addr", default="http://192.168.178.73"): str,
                 vol.Optional("update_interval", default=30): int,
             }
         )
@@ -39,3 +61,11 @@ class WaremaWMSWebControlConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
         parsed = urlparse(url)
         return all([parsed.scheme in ("http", "https"), parsed.netloc])
+
+class CannotConnect(exceptions.HomeAssistantError):
+    """Error to indicate we cannot connect."""
+
+
+class InvalidHost(exceptions.HomeAssistantError):
+    """Error to indicate there is an invalid hostname."""
+
