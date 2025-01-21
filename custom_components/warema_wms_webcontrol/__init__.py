b0c10134e247db598b097957746b5ff6b65e47a0
Hubert Becker <hu@hjpb.de>
2025-01-21 15:14:35 +0100

    Update __init__.py

--- custom_components/warema_wms_webcontrol/__init__.py
+++ custom_components/warema_wms_webcontrol/__init__.py
@@ -6,6 +6,8 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.const import Platform
 
+import logging
+
 # List of platforms to support. There should be a matching .py file for each,
 # eg <cover.py> and <sensor.py>
 PLATFORMS = [Platform.COVER]
