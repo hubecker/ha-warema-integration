4bf51259afaaff4c4bfd324d05f972256e6b10da
Hubert Becker <hu@hjpb.de>
2025-01-21 15:20:39 +0100

    Update __init__.py

--- custom_components/warema_wms_webcontrol/__init__.py
+++ custom_components/warema_wms_webcontrol/__init__.py
@@ -1,12 +1,11 @@
 """Warema WMS WebControl Integration."""
 
+import logging
 from __future__ import annotations
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.const import Platform
-
-import logging
 
 # List of platforms to support. There should be a matching .py file for each,
 # eg <cover.py> and <sensor.py>
