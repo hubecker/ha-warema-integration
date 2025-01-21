106cd1b3985af4487b6b5a915659e818d445ea6d
Hubert Becker <hu@hjpb.de>
2025-01-21 12:06:52 +0100

    Update const.py

--- custom_components/warema_wms_webcontrol/const.py
+++ custom_components/warema_wms_webcontrol/const.py
@@ -1,5 +1,5 @@
-DOMAIN: Final = "warema_wms_webcontrol"
-MANUFACTURE: Final = "Warema"
+DOMAIN = "warema_wms_webcontrol"
+MANUFACTURE = "Warema"
 DEFAULT_NAME = "lwarema"
 DEFAULT_HOST = "webcontrol"
 DEFAULT_SCAN_INTERVAL = 30
