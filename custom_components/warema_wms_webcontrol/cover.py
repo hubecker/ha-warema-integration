f24ac13d69bc86592d2c9d4796dda1872b3a321b
Hubert Becker <hu@hjpb.de>
2025-01-21 20:12:39 +0100

    Update cover.py

--- custom_components/warema_wms_webcontrol/cover.py
+++ custom_components/warema_wms_webcontrol/cover.py
@@ -23,12 +23,14 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
     url = config_entry.data["webcontrol_server_addr"]
     update_interval = config_entry.data["update_interval"]
 
-    logger.debug("URL: {}".url)
+    _LOGGER.debug.debug("URL: {}".format(url))
 
     # Create an instance of the WMSWebControl library
     wms_client = WmsController(url)
 
     shades = Shade.get_all_shades(wms_client, time_between_cmds=0.5)
+
+     _LOGGER.debug.debug("SHADES: {}".format(shades))
 
     async_add_entities(WaremaShade(s, update_interval) for s in shades)
 
